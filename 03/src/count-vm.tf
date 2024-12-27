4dac20abd3a0bf13f649feb7b1e9f7fc0b3957e7
Hellsong <hellsong@pes.com>
2024-12-27 17:11:15 +0700

    Homework 2.1

---
+++ 03/src/count-vm.tf
@@ -0,0 +1,31 @@
+data "yandex_compute_image" "ubuntu" {
+  family = "${var.vm_web_os_family}"
+}
+resource "yandex_compute_instance" "platform" {
+  count = 2
+  name        = "${var.vm_def_name}${count.index+1}"
+  platform_id = "${var.vm_web_platform}"
+  resources {
+    cores         = "${var.vm_web_resources.cores}"
+    memory        = "${var.vm_web_resources.memory}"
+    core_fraction = "${var.vm_web_resources.core_fraction}"
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    security_group_ids  = [yandex_vpc_security_group.example.id]
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = "${var.vms_meta.data.serial-port-enable}"
+    ssh-keys           = "${var.vms_meta.data.ssh-keys}"
+  }
+}
--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -1,16 +1,13 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
-
 variable "cloud_id" {
   type        = string
+  default     = "b1g6ufvpo7vkirq2qlm7"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1g7scrj5f0n2u2d9n3l"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -29,4 +26,42 @@ variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
+}
+
+variable "vm_web_os_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "OS Family"
+}
+
+variable "vm_web_platform" {
+  type        = string
+  default     = "standard-v2"
+  description = "VM Platform Id"
+}
+
+variable "vm_web_resources" {
+  type        = map(number)
+  default     = { cores = 2, memory = 1, core_fraction = 5 }
+  description = "VM Resources"
+}
+
+variable "vms_meta" {
+  type = map(object({
+    serial-port-enable = number
+    ssh-keys = string
+  }))
+  default = {
+    "data" = {
+      serial-port-enable = 1
+      ssh-keys = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMj9cW+g+/Nz7G8IgpTcUcupNyS7frO5j9e+7VSAkLp9"
+    }
+  }
+  
+}
+
+variable "vm_def_name" {
+  type        = string
+  default     = "web-"
+  description = "Default VM Name"
 }
