7f2b54a00a4bd3c5feb8cee69107a35a0c712358
Hellsong <hellsong@pes.com>
2024-12-30 19:28:29 +0700

    Задание 6

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -7,36 +7,27 @@ resource "local_file" "hosts_templatefile" {
 }
 
 resource "null_resource" "web_hosts_provision" {
-  #Ждем создания инстанса
   depends_on = [yandex_compute_instance.platform, yandex_compute_instance.platform2, yandex_compute_instance.platform3]
 
   #Добавление ПРИВАТНОГО ssh ключа в ssh-agent
   provisioner "local-exec" {
     command    = "> ~/.ssh/known_hosts &&eval $(ssh-agent) && cat ~/.ssh/ycservice | ssh-add -"
-    on_failure = continue #Продолжить выполнение terraform pipeline в случае ошибок
+    on_failure = continue
   }
 
-  #Костыль!!! Даем ВМ 60 сек на первый запуск. Лучше выполнить это через wait_for port 22 на стороне ansible
-  # # В случае использования cloud-init может потребоваться еще больше времени
-  # provisioner "local-exec" {
-  #   command = "sleep 60"
-  # }
+  provisioner "local-exec" {
+     command = "sleep 120"
+   }
 
   #Запуск ansible-playbook
   provisioner "local-exec" {
-    # without secrets
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${abspath(path.module)}/hosts.ini ${abspath(path.module)}/test.yml"
-    on_failure  = continue #Продолжить выполнение terraform pipeline в случае ошибок
+    on_failure  = continue
     environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
-    #срабатывание триггера при изменении переменных
   }
   triggers = {
-    always_run      = "${timestamp()}" #всегда т.к. дата и время постоянно изменяются
+    always_run      = "${timestamp()}"
     always_run_uuid = "${uuid()}"
-    # playbook_src_hash = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
-    # ssh_public_key = var.public_key # при изменении переменной with ssh
-    # template_rendered = "${local_file.hosts_templatefile.content}" #при изменении inventory-template
-    # password_change = jsonencode( {for k,v in random_password.each: k=>v.result})
 
   }
 
