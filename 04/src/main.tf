6e26086454ba310e6f493e90ba4c0e0e6a8ac94b
Hellsong <hellsong@pes.com>
2025-01-15 12:08:31 +0700

    Задание 1

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,10 +1,75 @@
+#создаем облачную сеть
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
+
+#создаем подсеть
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "${var.vpc_name}-a"
+  zone           = var.zone1
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+  v4_cidr_blocks = var.cidr1
 }
 
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "${var.vpc_name}-b"
+  zone           = var.zone2
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.cidr2
+}
+
+
+module "test-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a","ru-central1-b"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
+  instance_name  = "webs"
+  instance_count = 2
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "a.timofeev",
+    project = "marketing"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "example-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_name  = "web-stage"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "a.timofeev",
+    project = "analytics"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data template_file "cloudinit" {
+  template = file("./cloud-init.yml")
+
+  vars = {
+    ssh_public_key     = file(var.vms_ssh_root_key)
+  }
+}
--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -1,27 +1,37 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
-
 variable "cloud_id" {
   type        = string
+  default     = "b1g6ufvpo7vkirq2qlm7"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1g7scrj5f0n2u2d9n3l"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
-variable "default_zone" {
+variable "zone1" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
+
+variable "zone2" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "cidr1" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "cidr2" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
@@ -35,8 +45,7 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "your_ssh_ed25519_key"
-  description = "ssh-keygen -t ed25519"
+  default     = "~/.ssh/ycservice.pub"
 }
 
 ###example vm_web var
